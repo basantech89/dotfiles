dacc033345ead32a33eab4e27c55f39c2de9a572
Basant Soni <basantech89@gmail.com>
2025-04-28 22:58:19 +0530

    fix: fix android sdk related env variables

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,15 +4,6 @@ end
 
 starship init fish | source
 
-function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi "$argv" --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
-
 enable_transience
 
 function copyfile
